68de979a534be144237b52d7ce9ff8c9b1e79acd
Philippe Baurens <pbaurens.dev@gmail.com>
2020-09-05 12:02:01 +0200

    Connection API

--- Components/FilmItem.js
+++ Components/FilmItem.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { StyleSheet, View, Text, Image } from 'react-native'
-
+import { getImageFromApi } from '../API/TMDBApi'
 
 class FilmItem extends React.Component {
     render() {
@@ -9,7 +9,7 @@ class FilmItem extends React.Component {
             <View style={styles.main_container}>
                 <Image
                     style={styles.image}
-                    source={{uri: "image"}}
+                    source={{uri: getImageFromApi(film.poster_path)}}
                 />
                 <View style={styles.content_container}>
                     <View style={styles.header_container}>
@@ -30,10 +30,48 @@ class FilmItem extends React.Component {
 
 const styles = StyleSheet.create({
     main_container: {
-        height: 190
+        height: 190,
+        flexDirection: 'row'
     },
+    image: {
+        width: 120,
+        height: 180,
+        margin: 5,
+        backgroundColor: 'gray'
+    },
+    content_container: {
+        flex:1,
+        margin: 5
+    }, 
+    header_container: {
+        flex: 3,
+        flexDirection: 'row'
+    }, 
     title_text: {
-        
+        fontSize: 20,
+        fontWeight: 'bold',
+        flexWrap: 'wrap',
+        paddingRight: 5
+    },
+    vote_text: {
+        fontWeight:  'bold',
+        fontSize: 18,
+        color: '#666666'
+    },
+    description_container: {
+        flex: 7
+    },
+    description_text: {
+        fontStyle: 'italic',
+        color: '#666666'
+    },
+
+    date_container:{
+        flex: 1
+    },
+    date_text: {
+        textAlign: 'right',
+        fontSize: 14
     }
 })
 
--- Components/Search.js
+++ Components/Search.js
@@ -1,18 +1,55 @@
 
 
 import React from 'react'
-import { StyleSheet, View, Button, TextInput, FlatList, Text } from 'react-native'
-import films from '../Helpers/filmsData'
+import { StyleSheet, View, Button, TextInput, FlatList, Text, ActivityIndicator } from 'react-native'
 import FilmItem from './FilmItem'
+import { getFilmsFromApiWithSearchedText } from '../API/TMDBApi'
 
 class Search extends React.Component {
+
+    constructor(props) {
+        super(props)
+        this.state = { 
+            films: [],
+            isLoading: false
+        }
+            this.searchedText = ""
+    }
+
+    _loadFilms() {
+        this.setState({ isLoading: true })
+        if (this.searchedText.length > 0) { 
+            getFilmsFromApiWithSearchedText(this.searchedText).then(data =>
+                 this.setState({ 
+                     films: data.results,
+                     isLoading: false
+                    })
+                )   
+            }
+        }
+
+    _displayLoading() {
+        if ( this.state.isLoading) {
+            return (
+                <View style={styles.loading_container}>
+                    <ActivityIndicator size='large' />
+                </View>
+            )
+        }
+    }
+
+    _searchTextInputChanged(text) {
+        this.searchedText = text
+    }
+
     render() {
+        console.log("this.state.isLoading");
         return (
             <View style={styles.main_container}>
-                <TextInput style={styles.textinput} placeholder='Titre du film'/>
-                <Button title="Rechercher" onPress={() => {}}/>
+                <TextInput onSubmitEditing={() => this._loadFilms()} onChangeText={(text) => this._searchTextInputChanged(text)} style={styles.textinput} placeholder='Titre du film'/>
+                <Button style={{ height: 50 }}title="Rechercher" onPress={() => this._loadFilms()}/>
                 <FlatList
-                     data={films}
+                     data={this.state.films}
                      keyExtractor={(item) => item.id.toString()}
                      renderItem={({item}) => <FilmItem film={item}/>}
                  />
@@ -34,6 +71,15 @@ const styles = StyleSheet.create({
         borderWidth: 1, 
         paddingLeft: 5
     },
+    loading_container: {
+        position:  'absolute',
+        left: 0,
+        right: 0,
+        top: 100,
+        bottom: 0,
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
 })
 
 export default Search
